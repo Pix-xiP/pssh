391f2b5dfcf174c339622d2f756a747ca09bb642
Pix xiP <pix119@proton.me>
2025-12-24 16:39:33 +0800

    Loop for sketchy connections.

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
+	"fmt"
+	"os/exec"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/pix-xip/go-command"
+	"github.com/pix-xip/pssh/ssh"
 	"github.com/pix-xip/pssh/tui"
 )
 
@@ -19,6 +24,7 @@ func main() {
 	r := command.Root().Help("pssh is a TUI ssh manager").
 		Flags(func(fs *flag.FlagSet) {
 			fs.String("ssh-config", defaultSSHConfig, "path to ssh config file")
+			fs.Bool("loop", false, "loop until SSH connection successfully connects")
 		})
 
 	r.Action(RunTui)
@@ -34,16 +40,41 @@ func main() {
 
 func RunTui(_ context.Context, fs *flag.FlagSet, _ []string) error {
 	fp := command.Lookup[string](fs, "ssh-config")
+	loop := command.Lookup[bool](fs, "loop")
 
 	selectedHost := tui.SelectHost(fp)
 	if selectedHost != nil {
-		tmpl := "ssh {{.Name}}" // TODO: make this configurable
-		// IMPROV: could also add a {{.Comamand}} from CLI to run commnds via connection?
+		return runSSH(selectedHost, loop)
+	}
 
-		err := selectedHost.RunCmdTmpl(tmpl)
-		if err != nil {
-			return err
+	return nil
+}
+
+func runSSH(host *ssh.Host, loop bool) error {
+	tmpl := "ssh {{.Name}}" // TODO: make this configurable
+	// IMPROV: could also add a {{.Comamand}} from CLI to run commnds via connection?
+
+	if !loop {
+		return host.RunCmdTmpl(tmpl)
+	}
+
+	for {
+		err := host.RunCmdTmpl(tmpl)
+		if err == nil {
+			log.Info("Connection closed.")
+			break
 		}
+
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			// This is an expected error from ssh, so we can retry.
+			log.Infof("Connection failed, retrying in 2 seconds. Press Ctrl+C to cancel.")
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		// This is an unexpected error, so we should stop.
+		return fmt.Errorf("ssh command failed unexpectedly: %w", err)
 	}
 
 	return nil
